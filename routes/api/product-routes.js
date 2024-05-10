a68fece1414eb9fa65de51cd1198607d06371d87
Hunter <hunterde91@gmail.com>
2024-05-09 21:12:48 -0400

    fixed associations for product get

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category },{ model: Tag }],
+      include: [
+        { 
+          model: Category,
+          attributes:['id', 'category_name']
+        },
+        { 
+          model: Tag,
+          attributes:['id', 'tag_name']
+        }
+      ],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -24,7 +33,16 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category },{ model: Tag }],
+      include: [
+        { 
+          model: Category,
+          attributes:['id', 'category_name']
+        },
+        { 
+          model: Tag,
+          attributes:['id', 'tag_name']
+        }
+      ],
     });
 
     if (!productData) {
