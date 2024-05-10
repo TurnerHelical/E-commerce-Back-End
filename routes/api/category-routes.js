736408146c230c881d3731928f3748bf89ce9776
Hunter <hunterde91@gmail.com>
2024-05-09 21:55:54 -0400

    fixed tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,7 +65,6 @@ router.put('/:id', async (req, res) => {
 
 // delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
       where: {
