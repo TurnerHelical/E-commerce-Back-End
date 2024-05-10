822c45b15ad208f5adfa65b5df1bfae37c16e758
Hunter <hunterde91@gmail.com>
2024-05-09 21:43:52 -0400

    fixed product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -147,7 +147,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(productData);
+    res.status(200).json({message: 'Product has been deleted!'});
   } catch (err) {
     res.status(500).json(err);
   }
