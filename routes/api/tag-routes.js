5ddb1da82c108371c95bf0797ab135c9427483a2
Hunter <hunterde91@gmail.com>
2024-05-09 21:52:04 -0400

    fixed category routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,25 +45,21 @@ router.post('/', async (req, res) => {
 });
 
 // update a tag's name by its `id` value
-router.put('/:id', (req, res) => {
-  Tag.update(
-    {
-      // All the fields you can update and the data attached to the request body.
-      tag_name: req.body.title,
-    },
-    {
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
-    }
-  )
-    .then((updatedTag) => {
-      res.json(updatedTag);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json(err);
     });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
  // delete on tag by its `id` value
